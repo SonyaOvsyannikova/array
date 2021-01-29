742d631fa4a578f8f9460668eb70e67915b42ee7
asofa143@gmail.com <asofa143@gmail.com>
2021-01-29 20:18:05 +0400

    здравствуйте

--- array.cpp
+++ array.cpp
@@ -2,8 +2,34 @@
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "ru");
+	const int m = 5;
+	int* arr = new int[m];
+	cout << "заполните массив" << endl;
+	for (int i = 0; i < m; i++) {
+		cout << " Элемент № "<< i+1 << endl;
+		cin >> arr[i];
+		
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+
+	cout << endl;
+	int sum = arr[0];
+	cout << "[";
+	for (int i = 0; i < m; i++)
+	{ 
+		cout << "  " << sum;
+		sum += arr[i];
+        
+	}
+	cout << "]";
 }
 
